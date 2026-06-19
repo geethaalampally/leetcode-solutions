ce9737c19d0ecc7ece5d4f578a9b63090d58a5c1
geethaalampally <alampallygeetha62@gmail.com>
2026-06-19 08:37:50 +0530

    Time: 462 ms (78.02%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,8 +1,14 @@
-select customer_number from 
-(
-select customer_number , count(*) as total_orders 
-from orders
-group by customer_number 
-) t
-order by t.total_orders desc 
-limit 1;
+-- select customer_number from 
+-- (
+-- select customer_number , count(*) as total_orders 
+-- from orders
+-- group by customer_number 
+-- ) t
+-- order by t.total_orders desc 
+-- limit 1;
+
+SELECT customer_number
+FROM Orders 
+GROUP BY customer_number
+ORDER BY count(customer_number) DESC
+LIMIT 1
