2f5576d86fac8b03f3fcf8f8423ee30232567299
geethaalampally <alampallygeetha62@gmail.com>
2026-06-21 14:33:10 +0530

    Time: 348 ms (22.90%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,9 @@
+select id , count(*) as num
+from (
+select requester_id as id 
+from RequestAccepted
+union all 
+select accepter_id as id  from RequestAccepted) t
+group by id 
+order by count(id) desc
+limit 1;
