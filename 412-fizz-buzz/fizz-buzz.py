1c6ecad1ada4753afda318fb7d38fca4b5f8548c
geethaalampally <alampallygeetha62@gmail.com>
2026-06-28 18:23:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (85.61%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,31 +1,16 @@
-# class Solution:
-#     def fizzBuzz(self, n: int) -> List[str]:
-#         res=[]
-#         for i in range(1,n+1):
-#             val=""
-#             if i%15==0:
-#                 val="FizzBuzz"
-#             elif i%3==0:
-#                 val="Fizz"
-#             elif i%5==0:
-#                 val="Buzz"
-#             else:
-#                 val=str(i)
-#             res.append(val)
-#         return res
-        
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        result=[]
-        for i in range(1,n+1) :
-
-            if i%3==0 and i%5==0:
-                result.append("FizzBuzz")
+        res=[]
+        for i in range(1,n+1):
+            val=""
+            if i%15==0:
+                val="FizzBuzz"
             elif i%3==0:
-                result.append("Fizz")
+                val="Fizz"
             elif i%5==0:
-                result.append("Buzz")
+                val="Buzz"
             else:
-                result.append(str(i))
-        return result 
+                val=str(i)
+            res.append(val)
+        return res
         
